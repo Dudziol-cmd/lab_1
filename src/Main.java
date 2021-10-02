76214cfb5520af24b665a5e688392a1aa04c932b
Kacper Dudzik <plmplmoknokn12@gmail.com>
2021-10-02 19:02:20 +0200

    Zabawa z ćw4

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
 
+        int myFirstPrime = 3;
+        int mySecondPrime = 17;
+        int myThirdprime = 19;
+
+        float funWithPrimes = (((myFirstPrime * mySecondPrime) / myThirdprime) % (mySecondPrime | myFirstPrime)) * myThirdprime - myFirstPrime;
+
+        System.out.println(funWithPrimes);
     }
 }

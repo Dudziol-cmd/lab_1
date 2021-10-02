356a7b1fe536ef27cde8b41bf8dd8fc98e865eb4
Kacper Dudzik <plmplmoknokn12@gmail.com>
2021-10-02 18:32:50 +0200

    Zabawa z ćw1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        boolean mrBool = true;
+        short shorty = 32000;
+        int integer = 2100000;
+        long longest = 99999999;
 
+        System.out.println(mrBool);
+        System.out.println(shorty);
+        System.out.println(integer);
+        System.out.println(longest);
+
+        System.out.println("Long variable is: " + longest + " haha");
+        long longerLongest = longest + integer + shorty;
+        System.out.println("But I found longer longest! It's: " + longerLongest);
     }
 }

9e645035f9a85ce84ca70549cde16804b12880d8
Kacper Dudzik <plmplmoknokn12@gmail.com>
2021-10-02 18:56:45 +0200

    Zabawa z ćw3

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,34 @@ public class Main {
         *   wynik wyświetlić w konsoli programu
         */
 
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String surname;
+        int age;
+        int id;
+
+        System.out.println("Tell me your name, please.");
+        name = scanner.next();
+        System.out.println("Tell me your surname, please.");
+        surname = scanner.next();
+        System.out.println("Tell me something about your age.");
+        age = scanner.nextInt();
+        System.out.println("What is your ID?");
+        id = scanner.nextInt();
+
+        System.out.println("I've hacked some data about you!");
+        System.out.printf(name + " " + surname + " " + age + " " + id);
+
+        System.out.println();
+
+        int myIntA = 13;
+        int myIntB = 17;
+        System.out.println("I have done some primes calculations!");
+        System.out.printf("Sum: " + (myIntA + myIntB));
+        System.out.printf(" Difference: " + (myIntA - myIntB));
+        System.out.printf(" Multiplication: " + (myIntA * myIntB));
+        System.out.printf(" Division: " + (myIntA / myIntB));
+        System.out.printf(" Modulo: " + (myIntA % myIntB));
+        System.out.println("Good luck doing some encryption!");
     }
 }
